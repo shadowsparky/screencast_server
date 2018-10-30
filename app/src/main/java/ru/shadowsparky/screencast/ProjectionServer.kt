0fc0eb5a4a3ed001831e0510f410a425b133bce6
Eugene <samsonov_eugene@mail.ru>
2018-10-30 19:57:48 +0300

    Добавление функций сервера

--- app/src/main/java/ru/shadowsparky/screencast/ProjectionServer.kt
+++ app/src/main/java/ru/shadowsparky/screencast/ProjectionServer.kt
@@ -7,16 +7,27 @@ import android.content.Context
 import android.content.Intent
 import android.media.projection.MediaProjectionManager
 import android.os.IBinder
+import kotlinx.coroutines.experimental.Dispatchers
+import kotlinx.coroutines.experimental.GlobalScope
+import kotlinx.coroutines.experimental.IO
+import kotlinx.coroutines.experimental.async
 import ru.shadowsparky.screencast.Utils.Constants.Companion.DATA
 import ru.shadowsparky.screencast.Utils.Constants.Companion.DEFAULT_NOTIFICATION_ID
+import ru.shadowsparky.screencast.Utils.Constants.Companion.DEFAULT_PORT
 import ru.shadowsparky.screencast.Utils.Injection
 import ru.shadowsparky.screencast.Utils.Logger
 import ru.shadowsparky.screencast.Utils.Notifications
+import java.io.DataOutputStream
+import java.net.ServerSocket
+import java.net.Socket
 
 @SuppressLint("Registered")
 class ProjectionServer : Service() {
     private lateinit var mData: Intent
     private lateinit var mProjectionManager: MediaProjectionManager
+    private var mServerSocket: ServerSocket? = null
+    private var mClientSocket: Socket? = null
+    private var mClientStream: DataOutputStream? = null
     private val log: Logger = Injection.provideLogger()
 
     override fun onBind(intent: Intent?): IBinder? = null
@@ -28,7 +39,16 @@ class ProjectionServer : Service() {
         val notificationService = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val notification = Notifications(this).provideNotification(notificationService)
         startForeground(DEFAULT_NOTIFICATION_ID, notification)
-        log.printDebug("FOREGROUND STARTED")
+        startServer()
         return START_NOT_STICKY
+    }
+
+    fun startServer() = GlobalScope.async(Dispatchers.IO) {
+        mServerSocket = ServerSocket(DEFAULT_PORT)
+        log.printDebug("Waiting connection...")
+        mClientSocket = mServerSocket!!.accept()
+        log.printDebug("CONNECTION ACCEPTED")
+        mClientStream = DataOutputStream(mClientSocket!!.getOutputStream())
+
     }
 }
--- app/src/main/java/ru/shadowsparky/screencast/Utils/Constants.kt
+++ app/src/main/java/ru/shadowsparky/screencast/Utils/Constants.kt
@@ -6,6 +6,7 @@ class Constants {
         val TAG = "MAIN_TAG"
         val REQUEST_CODE = 77
         val DATA = "data"
+        val DEFAULT_PORT = 1337
         val DEFAULT_NOTIFICATION_CHANNEL = "Projection_Channel"
         val DEFAULT_NOTIFICATION_CHANNEL_NAME = "Проецирование на экран"
         val DEFAULT_NOTIFICATION_ID = 228
