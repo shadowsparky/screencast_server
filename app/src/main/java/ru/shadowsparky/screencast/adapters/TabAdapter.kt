5c0a4b8cc00a4ac56031ec54e1b7db263f651f47
shadowsparky <shadowsparky@github.com>
2019-05-24 00:02:38 +0300

    doc

--- app/src/main/java/ru/shadowsparky/screencast/adapters/TabAdapter.kt
+++ app/src/main/java/ru/shadowsparky/screencast/adapters/TabAdapter.kt
@@ -10,11 +10,24 @@ import androidx.fragment.app.FragmentStatePagerAdapter
 import ru.shadowsparky.screencast.views.MainFragment
 import ru.shadowsparky.screencast.views.SettingsFragment
 
+/**
+ * Используется для вставки фрагментов в Tab. Исключительно для дизайна.
+ *
+ * @param manager менеджер фраментов, использующийся для "вставки" фрагметов в Tab
+ * @param tab_count количество табов
+ * @constructor Создает Tab адаптер
+ */
 class TabAdapter(
         manager: FragmentManager,
         private val tab_count: Int
 ) : FragmentStatePagerAdapter(manager) {
 
+    /**
+     * Получает текущий элемент Tab'a
+     *
+     * @param position позиция Tab'a
+     * @return возвращает фрагмент, являющийся текущим элементом таба
+     */
     override fun getItem(position: Int): Fragment {
         val fragment: Fragment = when(position) {
             0 -> MainFragment()
@@ -25,6 +38,11 @@ class TabAdapter(
         return fragment
     }
 
+    /**
+     * Возвращает количество табов
+     *
+     * @return возвращает количество табов
+     */
     override fun getCount(): Int {
         return tab_count
     }
